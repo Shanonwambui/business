71e642b2975bef12710271d6de10574fed0bf9ef
Leakey Luse <31867006+leakeyluse@users.noreply.github.com>
2023-07-20 13:50:12 +0300

    Fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
 import { CartItem } from './cart/cart.model';
 import {environment} from "../environments/environment";
 import {MyService} from "./my.service";
@@ -14,13 +14,20 @@ import {ActivatedRoute} from "@angular/router";
 export class AppComponent implements OnInit{
   title = 'testing5';
 
-
+  showBusiness = false;
 
   Business: Business = {name: "", id: "",repemail: "",repmobile: ""};
 
-  constructor(private service: MyService, private route: ActivatedRoute) {}
+  constructor(private service: MyService, private route: ActivatedRoute, private router: Router) {}
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id') || environment.businessId;
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = this.router.url;
+        this.showBusiness = currentRoute.includes('/categories') || currentRoute.includes('/items') || currentRoute.includes('/cart') || currentRoute.includes('/home')|| currentRoute.includes('/contact-us') || currentRoute.includes('/checkout');
+      }
+    });
+
 
 
     this.service.getBusinessId().subscribe(
